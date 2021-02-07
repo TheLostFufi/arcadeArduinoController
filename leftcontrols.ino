bc54500d72ae0b98baa61b2ac676fb7da21322ae
the lost fufi dev <77300579+TheLostFufi@users.noreply.github.com>
2021-02-07 17:49:00 +0200

    add base fiels

---
+++ leftcontrols.ino
@@ -0,0 +1,56 @@
+// Simple example application that shows how to read four Arduino
+// digital pins and map them to the USB Joystick library.
+//
+// Ground digital pins 9, 10, 11, and 12 to press the joystick 
+// buttons 0, 1, 2, and 3.
+//
+// NOTE: This sketch file is for use with Arduino Leonardo and
+//       Arduino Micro only.
+//
+// by Matthew Heironimus
+// 2015-11-20
+//--------------------------------------------------------------------
+
+#include <Joystick.h>
+
+Joystick_ Joystick;
+
+void setup() {
+  // Initialize Button Pins
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+  pinMode(16, INPUT_PULLUP);
+
+  // Initialize Joystick Library
+  Joystick.begin();
+}
+
+// Constant that maps the phyical pin to the joystick button.
+const int pinToButtonMap = 2;
+
+// Last state of the button
+int ardPin[10] = {2,3,4,5,6,7,8,9,10,16};
+int lastButtonState[10] = {0,0,0,0,0,0,0,0,0,0};
+
+void loop() {
+
+  // Read pin values
+  for (int index = 0; index < 10; index++)
+  {
+    int currentButtonState = !digitalRead(ardPin[index]);
+    if (currentButtonState != lastButtonState[index])
+    {
+      Joystick.setButton(index, currentButtonState);
+      lastButtonState[index] = currentButtonState;
+    }
+  }
+
+  delay(50);
+}
